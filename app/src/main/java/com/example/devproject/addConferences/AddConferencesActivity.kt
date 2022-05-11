e28ce8536a9f2c74a461bee2a01cc36728926e95
jundonghyun <jundonghyun>
2022-05-11 15:49:10 +0900

    - 메인리스트에서 섬네일이 보이도록 변경 - DataHandler에서 이미지리스트를 받도록 추가

--- app/src/main/java/com/example/devproject/addConferences/AddConferencesActivity.kt
+++ app/src/main/java/com/example/devproject/addConferences/AddConferencesActivity.kt
@@ -22,6 +22,7 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.core.content.FileProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.devproject.R
@@ -88,10 +89,9 @@ class AddConferencesActivity() : AppCompatActivity() {
         }
 
         binding.addConImageBtn.setOnClickListener { //사진 불러오기
-            val intent = Intent(Intent.ACTION_PICK)
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             intent.type = MediaStore.Images.Media.CONTENT_TYPE
             intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-            intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
             startGetImageResult.launch(intent)
         }
 
@@ -169,7 +169,9 @@ class AddConferencesActivity() : AppCompatActivity() {
             if(checkInput(conference)){
                 if(storageWrite(docNumText, snapshotImage, imageList, "conferenceDocument", docNumText, conference)){
                     Toast.makeText(this, "업로드했습니다", Toast.LENGTH_SHORT).show()
-                    DataHandler.reload()
+                    CoroutineScope(Dispatchers.Main).launch {
+                        DataHandler.reload()
+                    }
                     finish()
                 }
             }else Toast.makeText(this, "빈칸을 모두 채워 주세요", Toast.LENGTH_SHORT).show()
