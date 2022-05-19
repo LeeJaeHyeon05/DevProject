7eb7ab605c5530e5247932d2f6d729eaaa79c46d
volta2030 <volta2030@gmail.com>
2022-05-19 22:29:17 +0900

    -Refactoring HomeFragment

--- app/src/main/java/com/example/devproject/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/devproject/fragment/HomeFragment.kt
@@ -7,15 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.devproject.activity.conference.AddConferencesActivity
 import com.example.devproject.databinding.FragmentHomeBinding
 import com.example.devproject.others.DBType
+import com.example.devproject.others.ListAdapter
+import com.example.devproject.others.StudyListAdapter
 import com.example.devproject.util.DataHandler
 import com.example.devproject.util.FirebaseIO
 import com.example.devproject.util.UIHandler
 
 class HomeFragment : Fragment() {
     private var mBinding : FragmentHomeBinding? = null
+
+    companion object{
+        var adapter : ListAdapter? = null
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,23 +34,19 @@ class HomeFragment : Fragment() {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         mBinding = binding
+
+        var conferRecyclerView = mBinding?.conferRecyclerView
+        conferRecyclerView?.layoutManager = LinearLayoutManager(this.context)
+        adapter = ListAdapter()
+        conferRecyclerView?.adapter = adapter
+
         val swipeRefreshLayout= mBinding?.swiperConferRefreshLayout
         swipeRefreshLayout?.setOnRefreshListener {
             DataHandler.reload(DBType.CONFERENCE)
             swipeRefreshLayout.isRefreshing = false
         }
-        UIHandler.allocateUI(mBinding?.root!!.rootView)
-        UIHandler.activateUI(mBinding?.conferRecyclerView!!.id)
-        addConferences()
-        return mBinding?.root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        mBinding = null
-    }
 
-    private fun addConferences() {
         val addCon = mBinding?.conferAddButton
         addCon?.setOnClickListener {
             if(FirebaseIO.isValidAccount()){
@@ -51,5 +56,12 @@ class HomeFragment : Fragment() {
                 Toast.makeText(this.context, "로그인이 필요합니다", Toast.LENGTH_SHORT).show()
             }
         }
+
+        return mBinding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mBinding = null
     }
 }
