b4f660cd18d6383b9b53bc0ca26faf91c0ed1337
LeeJaeHyeon <jaehyeon0825@gmail.com>
2022-06-14 18:11:06 +0700

    chip data

--- app/src/main/java/com/example/devproject/format/ConferenceInfo.kt
+++ app/src/main/java/com/example/devproject/format/ConferenceInfo.kt
@@ -4,7 +4,7 @@ import android.net.Uri
 import com.google.firebase.firestore.GeoPoint
 
 data class ConferenceInfo(
-    val documentID : String? = null,
+    val documentID: String? = null,
     var conferenceURL: String? = null,
     var content: String? = null,
     var date: String? = "",
@@ -12,9 +12,10 @@ data class ConferenceInfo(
     var place: String? = "",
     var price: Long? = null,
     var title: String? = null,
-    var uploader : String? = null,
+    var uploader: String? = null,
     val image: MutableList<Uri>? = null,
-    var uid : String? = null,
-    var startDate : String? = null,
-    var finishDate : String?  = null,
+    var uid: String? = null,
+    var startDate: String? = null,
+    var finishDate: String?  = null,
+    var tag: String? = null
 )
